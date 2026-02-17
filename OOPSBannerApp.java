afae154113859105eb2dc705f1051be2bbac5775
Shashank-ghub1102 <shashankramesh1102@gmail.com>
2026-02-17 10:38:53 +0530

    Updated OOPS banner design

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -10,12 +10,13 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(" ***     ***    ******    ***** ");
-        System.out.println("*   *   *   *   *     *  *     *");
-        System.out.println("*   *   *   *   *     *  *      ");
-        System.out.println("*   *   *   *   ******    ***** ");
-        System.out.println("*   *   *   *   *              *");
-        System.out.println("*   *   *   *   *        *     *");
-        System.out.println(" ***     ***    *         ***** ");
+        System.out.println("   ***        ***      *******      ******");
+        System.out.println(" **   **    **   **    **      **    **     ");
+        System.out.println("**     **  **     **  **      **    **     ");
+        System.out.println("**     **  **     **  **      **    ****** ");
+        System.out.println("**     **  **     **  **********         **");
+        System.out.println("**     **  **     **  **                   ");
+        System.out.println(" **   **    **   **    **              **  ");
+        System.out.println("   ***        ***      **           ****** ");
     }
 }
---
+++ src/OOPSBannerApp.java
@@ -0,0 +1,20 @@
+/**
+ * OOPSBannerApp
+ * Displays OOPS in Banner Format.
+ *
+ * @author Shashank_R
+ * @version 2.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****    *****   ******    ***** ");
+        System.out.println("*     *  *     *  *     *  *      ");
+        System.out.println("*     *  *     *  ******    ***** ");
+        System.out.println("*     *  *     *  *              *");
+        System.out.println("*     *  *     *  *              *");
+        System.out.println("*     *  *     *  *              *");
+        System.out.println(" *****    *****   *         ***** ");
+    }
+}
