a2c82539c1eac11a432e2e3883d773b8f2054af7
Shashank-ghub1102 <shashankramesh1102@gmail.com>
2026-02-17 10:21:57 +0530

    Fixed OOPS banner design

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -5,16 +5,17 @@
  * @author Shashank_R
  * @version 2.0
  */
+
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(" *****    *****   ******    ***** ");
-        System.out.println("*     *  *     *  *     *  *      ");
-        System.out.println("*     *  *     *  ******    ***** ");
-        System.out.println("*     *  *     *  *              *");
-        System.out.println("*     *  *     *  *              *");
-        System.out.println("*     *  *     *  *              *");
-        System.out.println(" *****    *****   *         ***** ");
+        System.out.println(" ***     ***    ******    ***** ");
+        System.out.println("*   *   *   *   *     *  *     *");
+        System.out.println("*   *   *   *   *     *  *      ");
+        System.out.println("*   *   *   *   ******    ***** ");
+        System.out.println("*   *   *   *   *              *");
+        System.out.println("*   *   *   *   *        *     *");
+        System.out.println(" ***     ***    *         ***** ");
     }
 }
