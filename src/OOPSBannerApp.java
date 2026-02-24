db05419fd7bca81fd1927ff578b281c31c7354e2
Shashank <shashankramesh1102@gmail.com>
2026-02-24 20:21:03 +0530

    Update OOPSBannerApp.java

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -7,10 +7,10 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        // 1️⃣ Create String array for 7 banner lines
+        // 1️ Create String array for 7 banner lines
         String[] lines = new String[7];
 
-        // 2️⃣ Populate each line using String.join()
+        // 2️ Populate each line using String.join()
 
         lines[0] = String.join(" ",
                 " *** ", " *** ", "*****", "*****"
@@ -40,7 +40,7 @@ public class OOPSBannerApp {
                 " *** ", " *** ", "**    ", "*****"
         );
 
-        // 3️⃣ Use enhanced for-loop to print banner
+        // Use for-loop to print banner
         for (String line : lines) {
             System.out.println(line);
         }
